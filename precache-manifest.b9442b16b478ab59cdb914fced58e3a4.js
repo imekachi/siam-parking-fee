4b1a0fc54e636babf2cf0e137b4f26d680a5549f
imekachi <imekachi@gmail.com>
2019-06-15 18:11:34 +0700

    Updates

---
+++ precache-manifest.b9442b16b478ab59cdb914fced58e3a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c65b023f8b0d77cd690874a8c7b2ec7",
+    "url": "/siam-parking-fee/index.html"
+  },
+  {
+    "revision": "f9396426bed8adcba1ce",
+    "url": "/siam-parking-fee/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a68155c0cec738aa1804",
+    "url": "/siam-parking-fee/static/js/2.fcf6be14.chunk.js"
+  },
+  {
+    "revision": "f9396426bed8adcba1ce",
+    "url": "/siam-parking-fee/static/js/main.b11d9332.chunk.js"
+  },
+  {
+    "revision": "574f5028d959c27cfbec",
+    "url": "/siam-parking-fee/static/js/runtime~main.1f52f072.js"
+  }
+]);
