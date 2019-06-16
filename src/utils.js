622cac2bad3a80dbfb3e5f90a12921c451039725
imekachi <saran@dek-d.com>
2019-06-16 16:49:34 +0700

    add function `getDurationHrs()` to get duration between 2 date objects

--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,17 @@
 export function isArray(value) {
   return !!value && typeof value === 'object' && value.constructor.name === 'Array'
 }
+
+/**
+ * Get duration between two date objects
+ * NOTE: the duration will always be positive, which mean the arguments order won't affect the result
+ *
+ * @param {Date} dateStart
+ * @param {Date} [dateEnd = new Date()]
+ * @returns {number} duration
+ */
+export function getDurationHrs(dateStart, dateEnd = new Date()) {
+  const durationMillisec = Math.abs(dateEnd - dateStart)
+  // convert milliseconds to hours
+  return durationMillisec / 1000 / 60 / 60
+}
--- src/utils.test.js
+++ src/utils.test.js
@@ -1,4 +1,4 @@
-import { isArray } from './utils'
+import { getDurationHrs, isArray } from './utils'
 
 describe('utils/isArray()', () => {
   const specs = [
@@ -31,6 +31,47 @@ describe('utils/isArray()', () => {
   specs.forEach(({ input, output }) => {
     it(`should return ${JSON.stringify(output)}, when receive (${JSON.stringify(input)}`, () => {
       expect(isArray(input)).toBe(output)
+    })
+  })
+})
+
+describe('utils/getDurationHrs', () => {
+  const specs = [
+    {
+      input: [new Date('2019-01-01 10:00:000Z'), new Date('2019-01-01 11:00:000Z')],
+      output: 1,
+    },
+    {
+      input: [new Date('2019-01-01 11:00:000Z'), new Date('2019-01-01 10:00:000Z')],
+      output: 1,
+    },
+    {
+      input: [new Date('2019-01-01 10:00:000Z'), new Date('2019-01-01 10:30:000Z')],
+      output: 0.5,
+    },
+    {
+      input: [new Date('2019-01-01 10:00:000Z'), new Date('2019-01-01 10:15:000Z')],
+      output: 0.25,
+    },
+    {
+      input: [new Date('2019-01-01 10:00:000Z'), new Date('2019-01-02 10:00:000Z')],
+      output: 24,
+    },
+    {
+      input: [new Date('2019-01-01 10:00:000Z'), new Date('2019-01-01 15:15:000Z')],
+      output: 5.25,
+    },
+    {
+      input: [new Date('2019-01-01 10:00:000Z'), new Date('2019-01-01 6:15:000Z')],
+      output: 3.75,
+    },
+  ]
+
+  specs.forEach(({ input, output }) => {
+    it(`should return ${JSON.stringify(output)}, when receives (${JSON.stringify(
+      input[0]
+    )}, ${JSON.stringify(input[1])})`, () => {
+      expect(getDurationHrs(...input)).toBe(output)
     })
   })
 })
