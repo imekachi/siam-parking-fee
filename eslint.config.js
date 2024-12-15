ce8b9b378d30b7f1f40994d44fcfc2388f717540
imekachi <imekachi@gmail.com>
2024-12-15 21:47:07 +0700

    chore: migrate eslint to v19

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import js from '@eslint/js'
+import eslint from '@eslint/js'
 import stylistic from '@stylistic/eslint-plugin'
 import importPlugin from 'eslint-plugin-import'
 import prettier from 'eslint-plugin-prettier'
@@ -8,73 +8,104 @@ import unusedImports from 'eslint-plugin-unused-imports'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ['**/*.{ts,tsx,js,cjs,mjs}'],
-  ignores: ['dist', 'cypress/**/*'],
-  languageOptions: {
-    ecmaVersion: 2023,
-    globals: {
-      ...globals.browser,
-      React: true,
-      JSX: true,
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    ignores: ['dist/*', 'cypress/**/*'],
+  },
+  {
+    files: ['*.cjs'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
     },
   },
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-    import: importPlugin,
-    prettier,
-    'unused-imports': unusedImports,
-    '@stylistic': stylistic,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'no-param-reassign': 'warn',
-    'prettier/prettier': 'warn',
-    'no-console': 'off',
-    // no duplicate variable naming with outer scope
-    'no-shadow': 'off',
-    '@typescript-eslint/no-shadow': 'error',
-    // Import sorting
-    'import/order': [
-      'warn',
-      {
-        pathGroups: [{ pattern: '@/**', group: 'internal' }],
-        alphabetize: { order: 'asc', caseInsensitive: true },
-        'newlines-between': 'never',
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-        ],
+  {
+    files: ['**/*.{ts,tsx,js,cjs,mjs}'],
+    languageOptions: {
+      ecmaVersion: 2023,
+      globals: {
+        ...globals.browser,
+        React: true,
+        JSX: true,
       },
-    ],
-    'sort-imports': ['warn', { ignoreCase: true, ignoreDeclarationSort: true }],
-    // Remove unused imports
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    'unused-imports/no-unused-imports': 'warn',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-      },
-    ],
-    'no-implicit-coercion': 'warn',
-    'no-unneeded-ternary': 'warn',
-    'object-shorthand': 'warn',
-    curly: ['warn', 'multi-line', 'consistent'],
-    '@stylistic/spaced-comment': 'warn',
+    },
+    plugins: {
+      prettier,
+      import: importPlugin,
+      'unused-imports': unusedImports,
+      '@stylistic': stylistic,
+    },
+    rules: {
+      'no-param-reassign': 'warn',
+      'prettier/prettier': 'warn',
+      'no-console': 'off',
+      'import/order': [
+        'warn',
+        {
+          pathGroups: [{ pattern: '@/**', group: 'internal' }],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          'newlines-between': 'never',
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+        },
+      ],
+      'sort-imports': [
+        'warn',
+        { ignoreCase: true, ignoreDeclarationSort: true },
+      ],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'warn',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+
+      'no-implicit-coercion': 'warn',
+      'no-unneeded-ternary': 'warn',
+      'object-shorthand': 'warn',
+      curly: ['warn', 'multi-line', 'consistent'],
+      '@stylistic/spaced-comment': 'warn',
+    },
   },
-})
+  // JS
+  {
+    files: ['**/*.{js,cjs,mjs}'],
+    rules: {
+      // no duplicate variable naming with outer scope
+      'no-shadow': 'error',
+    },
+  },
+  // TS, React
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      // no duplicate variable naming with outer scope
+      'no-shadow': 'off',
+      '@typescript-eslint/no-shadow': 'error',
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+)
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 export default {
   'src/**/*.{ts,tsx}': () => 'pnpm run type-check',
-  '**/*.{ts,tsx,js,jsx,cjs,mjs}': 'eslint --fix --max-warnings 0',
-  '**/*.{ts,tsx,js,jsx,cjs,mjs}': 'vitest related --run',
+  '**/*.{ts,tsx,js,jsx,cjs,mjs,__parallel1__}': 'eslint --fix --max-warnings 0',
+  '**/*.{ts,tsx,js,jsx,cjs,mjs,__parallel2__}': 'vitest related --run',
 }
